31e9b1d509e48a695196dfc17b53594438babf43
Industrylaud <mrfros22@gmail.com>
2020-04-07 20:32:20 +0200

    add mvc options and app authentication

--- AltPro.BackTracker/Startup.cs
+++ AltPro.BackTracker/Startup.cs
@@ -40,9 +40,9 @@ namespace AltPro.BackTracker
             });
 
             services.AddMvc(options => {
-                //var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
                 options.EnableEndpointRouting = false;
-                //options.Filters.Add(new AuthorizeFilter(policy));
+                options.Filters.Add(new AuthorizeFilter(policy));
             });
         }
 
@@ -62,7 +62,7 @@ namespace AltPro.BackTracker
 
             app.UseStaticFiles();
 
-            //app.UseAuthentication();
+            app.UseAuthentication();
 
             app.UseMvc(routes => 
             { 
