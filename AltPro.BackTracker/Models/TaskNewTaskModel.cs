6d1d02aee0794784953adc28d14cea9508c3e319
SiarXx <siarxxpl@gmail.com>
2020-04-12 14:56:08 +0200

    Simple Task Model

---
+++ AltPro.BackTracker/Models/TaskNewTaskModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltPro.BackTracker.Models
+{
+    public class TaskNewTaskModel
+    {
+        [Required]
+        public int TaskID { get; set; }
+        public string ModuleName { get; set; }
+
+        [Required]
+        public string ReporterID { get; set; }
+
+        public string TaskPriority { get; set; }
+        public string TaskStatus { get; set; }
+
+    }
+}
