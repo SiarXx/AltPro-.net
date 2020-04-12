8ba295e6ab54722d36f7117201dedea54f556605
SiarXx <siarxxpl@gmail.com>
2020-04-12 16:12:54 +0200

    add Task Controller, Bindied nw task view to task controller

---
+++ AltPro.BackTracker/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltPro.BackTracker.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult NewTask()
+        {
+            return View();
+        }
+    }
+}
