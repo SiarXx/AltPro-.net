43ce637baef3bdb0f4be1d0e7c46807eb898d3c0
SiarXx <siarxxpl@gmail.com>
2020-04-21 12:54:13 +0200

    Task View Sample

--- AltPro.BackTracker/Controllers/TaskController.cs
+++ AltPro.BackTracker/Controllers/TaskController.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AltPro.BackTracker.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AltPro.BackTracker.Controllers
 {
     public class TaskController : Controller
     {
+        TaskViewModel newTask = new TaskViewModel();
         [HttpGet]
         public IActionResult NewTask()
         {
-            return View();
+            newTask.ReporterID = "SampleReporter2137";
+            return View(newTask);
         }
     }
 
