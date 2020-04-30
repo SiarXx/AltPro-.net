56fe5888c2dbd1b29c11672c88391b408f0cf00d
Kacper Błażejewski <siarxxpl@gmail.com>
2020-04-30 14:28:35 +0200

    ITaskRepository

--- AltPro.BackTracker/Models/SQLTaskRepository.cs
+++ AltPro.BackTracker/Models/SQLTaskRepository.cs
@@ -5,7 +5,31 @@ using System.Threading.Tasks;
 
 namespace AltPro.BackTracker.Models
 {
-    public class SQLTaskRepository
+    public class SQLTaskRepository : ITaskRepository
     {
+        public TaskModel Add(TaskModel taskModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskModel Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskModel Edit(TaskModel taskModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskModel> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskModel GetTask(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
