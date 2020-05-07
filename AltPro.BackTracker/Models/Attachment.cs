958031261bc6d16c76811996f8cb2c13a709b067
Industrylaud <mrfros22@gmail.com>
2020-05-07 23:05:17 +0200

    add migration to data base with attachment table

--- AltPro.BackTracker/Models/Attachment.cs
+++ AltPro.BackTracker/Models/Attachment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +13,9 @@ namespace AltPro.BackTracker.Models
         public int Id { get; set; }
 
         [Required]
+        [ForeignKey("TaskModel")]
         public int TaskId { get; set; }
+        public TaskModel TaskModel {get; set;}
 
         [Required]
         public string Path { get; set; }
