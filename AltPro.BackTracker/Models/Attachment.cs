cc570f2456287b2d97a15bbcce8ac3b9f1255cce
Industrylaud <mrfros22@gmail.com>
2020-05-07 22:57:52 +0200

    add attachment model

---
+++ AltPro.BackTracker/Models/Attachment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltPro.BackTracker.Models
+{
+    public class Attachment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+    }
+}
